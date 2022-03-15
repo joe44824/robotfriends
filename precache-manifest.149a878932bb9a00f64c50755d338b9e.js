f2fd7300fa5ab8d7e63bbd638a6b33f5a929da1d
Joe Edwards <joe@joeeds.lan>
2022-03-15 17:56:39 +0800

    Updates

---
+++ precache-manifest.149a878932bb9a00f64c50755d338b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ce536d54cd5cfafd809",
+    "url": "/robotfriends/static/css/main.e240538a.chunk.css"
+  },
+  {
+    "revision": "5ce536d54cd5cfafd809",
+    "url": "/robotfriends/static/js/main.4e3a6658.chunk.js"
+  },
+  {
+    "revision": "704c8e190cebdc5fb86a",
+    "url": "/robotfriends/static/js/runtime~main.4a652d4e.js"
+  },
+  {
+    "revision": "7469332fe1afa0b2550f",
+    "url": "/robotfriends/static/css/2.628f4452.chunk.css"
+  },
+  {
+    "revision": "7469332fe1afa0b2550f",
+    "url": "/robotfriends/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/robotfriends/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2d3af43dea08f2f4a315455899128953",
+    "url": "/robotfriends/index.html"
+  }
+];
